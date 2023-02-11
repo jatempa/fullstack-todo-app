9887fd2e4296b1d3065a67c3dd28345d9ceefb71
Jorge Atempa <atempaitm@gmail.com>
2023-02-10 22:50:35 -0800

    Get tasks from server implements GraphQL

--- client/src/hooks/useTasks.jsx
+++ client/src/hooks/useTasks.jsx
@@ -1,26 +1,75 @@
-import React, { createContext, useState, useContext, useMemo } from 'react';
-import tasksData from '../data/tasks.json';
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useMemo,
+  useEffect,
+} from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
+const GET_ALL_TASKS = gql`
+  query GetAllTasks {
+    tasks {
+      id
+      title
+      done
+    }
+  }
+`;
 
 const TaskContext = createContext();
 
 export const useTasks = () => useContext(TaskContext);
 
 const TaskProvider = ({ children }) => {
-  const [tasks, setTasks] = useState(tasksData);
+  const [tasks, setTasks] = useState([]);
+  const [results, setResults] = useState({
+    all: 0,
+    complete: 0,
+    incomplete: 0,
+  });
 
-  const all = tasks.length;
-  const complete = tasks.filter((task) => task.done).length;
-  const incomplete = all - complete;
+  useEffect(() => {
+    client
+      .query({
+        query: GET_ALL_TASKS,
+      })
+      .then((t) => {
+        setTasks(t.data.tasks);
 
-  const results = {
-    all,
-    complete,
-    incomplete,
-  };
+        const all = tasks.length;
+        const complete = tasks.filter((task) => task.done).length;
+        const incomplete = all - complete;
 
-  const data = useMemo(() => ({ tasks, setTasks, results }), [tasks, results]);
+        setResults({
+          ...results,
+          all,
+          complete,
+          incomplete,
+        });
+      });
+  }, [tasks, results]);
 
-  return <TaskContext.Provider value={data}>{children}</TaskContext.Provider>;
+  const values = useMemo(
+    () => ({ tasks, setTasks, results }),
+    [tasks, results]
+  );
+
+  return (
+    <ApolloProvider client={client}>
+      <TaskContext.Provider value={values}>{children}</TaskContext.Provider>
+    </ApolloProvider>
+  );
 };
 
 export default TaskProvider;
